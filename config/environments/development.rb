c524f571f982d08007140d85853f31d6b2ddde3b
Malaika Ishtiaq <malaikamic@gmail.com>
2018-09-26 23:15:15 +0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,13 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'apikey',
-    password:       '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',
-    domain:         'smtp.sendgrid.net',
-    address:       'malaikamic@gmail.com.gmail.com',
-    port:          '465',
-    server: 'smtp.sendgrid.net',
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
     enable_starttls_auto: true
   }
   # In the development environment your application's code is reloaded on
